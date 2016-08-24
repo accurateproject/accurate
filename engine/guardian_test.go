397c8c84d5d9bebdfb44780f2e6fdb7725cc35a0
Radu Ioan Fericean <radu@fericean.ro>
2016-08-24 21:26:34 +0300

    test for guardian key eviction

--- engine/guardian_test.go
+++ engine/guardian_test.go
@@ -23,6 +23,27 @@ import (
 	"time"
 )
 
+func TestGuardianDelete(t *testing.T) {
+	for i := 0; i < 3; i++ {
+		go Guardian.Guard(func() (interface{}, error) {
+			time.Sleep(10 * time.Millisecond)
+			return 0, nil
+		}, 0, "1")
+	}
+	time.Sleep(11 * time.Millisecond)
+	if _, ok := Guardian.locksMap["1"]; !ok {
+		t.Error("Deleted after 11 milliseconds")
+	}
+	time.Sleep(11 * time.Millisecond)
+	if _, ok := Guardian.locksMap["1"]; !ok {
+		t.Error("Deleted after 22 milliseconds")
+	}
+	time.Sleep(11 * time.Millisecond)
+	if _, ok := Guardian.locksMap["1"]; ok {
+		t.Error("should be deleted by now")
+	}
+}
+
 func BenchmarkGuard(b *testing.B) {
 	for i := 0; i < 100; i++ {
 		go Guardian.Guard(func() (interface{}, error) {
